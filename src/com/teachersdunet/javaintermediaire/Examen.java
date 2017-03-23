c3950cb024ff1cece039a00249a26e52bd639144
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 16:40:43 +0100

    modification de la classe main

--- src/com/teachersdunet/javaintermediaire/Examen.java
+++ src/com/teachersdunet/javaintermediaire/Examen.java
@@ -1,8 +1,10 @@
 package com.teachersdunet.javaintermediaire;
 
 public class Examen {
-	public void afficherMessage(){
-		System.out.println("Bienvenue à cet examen");
+	public void afficherMessage(String matiere){
+		
+		//System.out.println("Bienvenue à cet examen");
+		System.out.printf("Bienvenue à cet examen de la matière %s", matiere);
 	}
 	
 }
--- src/com/teachersdunet/javaintermediaire/ExamenTest.java
+++ src/com/teachersdunet/javaintermediaire/ExamenTest.java
@@ -1,13 +1,18 @@
 package com.teachersdunet.javaintermediaire;
 
+import java.util.Scanner;
+
 public class ExamenTest {
 
 	public static void main(String[] args) {
+		Scanner clavier = new Scanner(System.in);
 		//String name = "Je suis une chaîne de caractères";
 		//System.out.println(name);
 		Examen examen = new Examen();
-		examen.afficherMessage();
-
+		System.out.println("Entrez la matière concernée: ");
+		String matiere = clavier.nextLine();
+		examen.afficherMessage(matiere);
+		clavier.close();
 	}
 
 }
