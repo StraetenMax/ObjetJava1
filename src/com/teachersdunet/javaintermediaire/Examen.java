6ac5cfa9b38fe85f99e1180055c81f04c6290b5c
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 15:47:11 +0100

    dépot du projet sur github

---
+++ src/com/teachersdunet/javaintermediaire/Examen.java
@@ -0,0 +1,8 @@
+package com.teachersdunet.javaintermediaire;
+
+public class Examen {
+	public void afficherMessage(){
+		System.out.println("Bienvenue à cet examen");
+	}
+	
+}
---
+++ src/com/teachersdunet/javaintermediaire/ExamenTest.java
@@ -0,0 +1,13 @@
+package com.teachersdunet.javaintermediaire;
+
+public class ExamenTest {
+
+	public static void main(String[] args) {
+		//String name = "Je suis une chaîne de caractères";
+		//System.out.println(name);
+		Examen examen = new Examen();
+		examen.afficherMessage();
+
+	}
+
+}
